b373bc4854210fb2972200186372bf999d36470a
Gaein_nidb <mail@gaein.cn>
2021-09-08 23:40:54 +0800

    Add folders and models

---
+++ TalkFreely.Backend/Entities/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TalkFreely.Backend.Entities
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public DateTime CommentTime { get; set; }
+        public string AuthorBrower { get; set; }
+        public string AuthorSystem { get; set; }
+        public Uri AuthorAvatar { get; set; }
+        public string AuthorMail { get; set; }
+    }
+}
---
+++ TalkFreely.Backend/Entities/Page.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TalkFreely.Backend.Entities
+{
+    public class Page
+    {
+        public Guid Id { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+    }
+}
